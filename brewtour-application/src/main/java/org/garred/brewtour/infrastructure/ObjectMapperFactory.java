f1a220ec164718400b2404a39f3a23acdcd5a978
Dave Garred <david.garred@openmarket.com>
2015-11-10 14:06:08 -0800

    update jackson to 2.6.3

--- brewtour-application/src/main/java/org/garred/brewtour/infrastructure/ObjectMapperFactory.java
+++ brewtour-application/src/main/java/org/garred/brewtour/infrastructure/ObjectMapperFactory.java
@@ -1,11 +1,13 @@
 package org.garred.brewtour.infrastructure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperFactory {
 
 	public static ObjectMapper objectMapper() {
 		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
 		return objectMapper;
 	}
 }
