e2343e67a5de1f637f9ad5673ef1e4df5a54ae63
Dave Garred <david.garred@openmarket.com>
2016-02-05 08:56:52 -0800

    update command initialization to use commands stored in the db

--- brewtour-infrastructure/src/main/java/org/garred/brewtour/config/command_init/CommandInitialization.java
+++ brewtour-infrastructure/src/main/java/org/garred/brewtour/config/command_init/CommandInitialization.java
@@ -1,18 +1,25 @@
 package org.garred.brewtour.config.command_init;
 
+import static java.lang.String.format;
+import static java.sql.Types.CLOB;
+import static java.sql.Types.VARCHAR;
 import static org.garred.brewtour.domain.AvailableImages.NO_IMAGES;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
+
+import javax.sql.DataSource;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.garred.brewtour.application.IdentifierFactory;
@@ -40,8 +47,13 @@ import org.garred.brewtour.domain.UserId;
 import org.garred.brewtour.infrastructure.ObjectMapperFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -49,10 +61,15 @@ public class CommandInitialization implements ApplicationListener<ContextRefresh
 
 	private final CommandGateway gateway;
 	private final IdentifierFactory<LocationId> identifierFactory;
+	private final JdbcTemplate jdbcTemplate;
+	private final ObjectMapper objectMapper = ObjectMapperFactory.objectMapper();
+	private final PreparedStatementCreatorFactory insert = new PreparedStatementCreatorFactory(
+			format("INSERT INTO commands(id,object_type,data) values (?,?,?)"), VARCHAR, VARCHAR, CLOB);
 
-	public CommandInitialization(CommandGateway gateway, IdentifierFactory<LocationId> identifierFactory) {
+	public CommandInitialization(CommandGateway gateway, IdentifierFactory<LocationId> identifierFactory, DataSource dataSource) {
 		this.gateway = gateway;
 		this.identifierFactory = identifierFactory;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 
@@ -64,18 +81,22 @@ public class CommandInitialization implements ApplicationListener<ContextRefresh
 	private final List<UpdateLocationPositionCommand> positionUpdates = new ArrayList<>();
 	private final List<UpdateLocationWebsiteCommand> websiteUpdates = new ArrayList<>();
 
-	private final Map<LocationId,Map<String,AddBeerCommand>> addBeerCommands = new HashMap<>();
+	private final Set<AddBeerCommand> addBeerCommands = new HashSet<>();
+//	private final Map<LocationId,Map<String,AddBeerCommand>> addBeerCommands = new HashMap<>();
 
 	private final Map<String, Beer> beerMap = new HashMap<>();
 
-	public void migrate() throws Exception {
-		final ObjectMapper objectMapper = ObjectMapperFactory.objectMapper();
-		loadBeerData(objectMapper);
+	public void buildCommandTable() throws Exception {
+		loadBeerData();
 		final InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("json/test_locations.json");
-		final BrewDbLocationList locations = objectMapper.readValue(in, BrewDbLocationList.class);
+		final BrewDbLocationList locations = this.objectMapper.readValue(in, BrewDbLocationList.class);
 		for (final BrewDbLocation brewDbLocation : locations) {
 			populateLists(brewDbLocation);
 		}
+		this.jdbcTemplate.execute(
+				"CREATE TABLE if not exists commands(id varchar(36) NOT NULL, " +
+				"  object_type varchar(100) NOT NULL, " +
+				"  data clob)");
 		push(this.addressUpdates);
 		push(this.descriptionUpdates);
 		push(this.hoursOfOperationUpdates);
@@ -86,14 +107,15 @@ public class CommandInitialization implements ApplicationListener<ContextRefresh
 		push(this.addBeerCommands);
 	}
 
-	private void push(Map<LocationId,Map<String, AddBeerCommand>> commands) {
-		for(final Entry<LocationId, Map<String, AddBeerCommand>> entry : commands.entrySet()) {
-			push(entry.getValue().values());
-		}
-	}
+//	private void push(Map<LocationId,Map<String, AddBeerCommand>> commands) {
+//		for(final Entry<LocationId, Map<String, AddBeerCommand>> entry : commands.entrySet()) {
+//			push(entry.getValue().values());
+//		}
+//	}
 
 	private void push(Collection<? extends AbstractLocationCommand> commands) {
 		try {
+			commands.stream().forEach(c -> storeInTable(c));
 			commands.stream().forEach(c -> this.gateway.send(c));
 		} catch(final Exception e) {
 			e.printStackTrace();
@@ -101,8 +123,10 @@ public class CommandInitialization implements ApplicationListener<ContextRefresh
 	}
 
 	private void populateLists(BrewDbLocation brewDbLocation) {
-		this.gateway.sendAndWait(new AddLocationCommand(brewDbLocation.brewery.name));
+		final AddLocationCommand command = new AddLocationCommand(brewDbLocation.brewery.name);
+		this.gateway.sendAndWait(command);
 		final LocationId locationId = this.identifierFactory.last();
+		storeInTable(locationId, command);
 
 		this.addressUpdates.add(new UpdateLocationAddressCommand(locationId, brewDbLocation.streetAddress, "",
 				brewDbLocation.locality, brewDbLocation.region, brewDbLocation.postalCode));
@@ -121,21 +145,23 @@ public class CommandInitialization implements ApplicationListener<ContextRefresh
 		this.websiteUpdates.add(new UpdateLocationWebsiteCommand(locationId, brewDbLocation.website));
 
 
-		final HashMap<String, AddBeerCommand> locationBeerMap = new HashMap<>();
-		this.addBeerCommands.put(locationId, locationBeerMap);
+//		final HashMap<String, AddBeerCommand> locationBeerMap = new HashMap<>();
+//		this.addBeerCommands.put(locationId, locationBeerMap);
 		if (brewDbLocation.beerIds != null) {
 			for (final String beerId : brewDbLocation.beerIds) {
 				final Beer beer = this.beerMap.get(beerId);
 				final String beerName = beer.getName();
-				locationBeerMap.put(beerName.trim().toLowerCase(), new AddBeerCommand(locationId, beerName, beer.getStyle(), beer.getCategory(), beer.getAbv(), beer.getIbu()));
+				final AddBeerCommand addBeerCommand = new AddBeerCommand(locationId, beerName, beer.getStyle(), beer.getCategory(), beer.getAbv(), beer.getIbu());
+//				locationBeerMap.put(beerName.trim().toLowerCase(), addBeerCommand);
+				this.addBeerCommands.add(addBeerCommand);
 			}
 		}
 	}
 
-	private void loadBeerData(ObjectMapper objectMapper) throws JsonParseException, JsonMappingException, IOException {
+	private void loadBeerData() throws JsonParseException, JsonMappingException, IOException {
 		final Set<Style> bdbStyles = new HashSet<>();
 		try (final InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("json/test_beers.json")) {
-			final BrewDbBeerList beers = objectMapper.readValue(in, BrewDbBeerList.class);
+			final BrewDbBeerList beers = this.objectMapper.readValue(in, BrewDbBeerList.class);
 			for (final BrewDbBeer brewDbBeer : beers) {
 				final Beer beer = convert(brewDbBeer);
 				bdbStyles.add(brewDbBeer.style);
@@ -151,13 +177,55 @@ public class CommandInitialization implements ApplicationListener<ContextRefresh
 		return result;
 	}
 
+	private void storeInTable(LocationId locationId, Object command) {
+		try {
+			final List<Object> params = new ArrayList<>();
+			params.add(locationId.id);
+			params.add(command.getClass().getName());
+			params.add(this.objectMapper.writeValueAsString(command));
+			final PreparedStatementCreator statement = this.insert.newPreparedStatementCreator(params);
+			this.jdbcTemplate.update(statement);
+		} catch (final JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	private void storeInTable(final AbstractLocationCommand command) {
+		storeInTable(command.locationId, command);
+	}
+
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		this.gateway.send(new AddUserCommand(new UserId(UUID.randomUUID().toString()), "dave", "dave"));
 		try {
-			migrate();
+//			buildCommandTable();
+			fireCommandsFromTable();
 		} catch (final Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private void fireCommandsFromTable() {
+		final String initCommandsQuery = "SELECT * FROM commands where object_type like '%AddLocationCommand'";
+		final List<Object> initcommands = queryCommands(initCommandsQuery);
+		initcommands.stream().forEach(c -> this.gateway.send(c));
+		final String otherCommandquery = "SELECT * FROM commands where object_type not like '%AddLocationCommand' group by id,object_type,data";
+		final List<Object> commands = queryCommands(otherCommandquery);
+		commands.stream().forEach(c -> this.gateway.send(c));
+	}
+
+	private List<Object> queryCommands(String otherCommands) {
+		return this.jdbcTemplate.query(otherCommands, new RowMapper<Object>() {
+			@Override
+			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+				try {
+					@SuppressWarnings("unchecked")
+					final Class<Object> type = (Class<Object>) Class.forName(rs.getString("object_type"));
+					final Reader reader = rs.getCharacterStream("data");
+					return CommandInitialization.this.objectMapper.readValue(reader, type);
+				} catch (final ClassNotFoundException | IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
 	}
 }
